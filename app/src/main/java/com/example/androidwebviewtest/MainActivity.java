2fc3335b9d838ea10886e3d7dfca6ac69633db3c
Jeonggyu Park <goodsogi@gmail.com>
2020-07-06 12:50:27 +0900

    1. 파일 다운로드 기능 추가 2. link 클릭시 크롬브라우저에서 열리는 버그 수정

--- app/src/main/java/com/example/androidwebviewtest/MainActivity.java
+++ app/src/main/java/com/example/androidwebviewtest/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.androidwebviewtest;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
 import android.webkit.JavascriptInterface;
+import android.webkit.URLUtil;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -25,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //버그인가? write external storage 권한이 있는데 외부저장소에 파일을 쓰면 강종됨
+        //무조건 한번 권한요청을 해야 하고 그러면 다음 실행할 때에는 무시됨
+        handleExternalStoragePermission();
+
+
         //WebView Object
 
         browser=(WebView)findViewById(R.id.webkit);
@@ -38,13 +55,141 @@ public class MainActivity extends AppCompatActivity {
         websettings.setJavaScriptEnabled(true);
 
         //alert가 작동하려면 아래 WebChromeClient 지정해야 함
+        browser.setWebViewClient(new WebViewClient());
         browser.setWebChromeClient(new WebChromeClient());
         browser.addJavascriptInterface(new AndroidBridge(), "android");
+        browser.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLength) {
 
-        browser.loadUrl("file:///android_asset/www/test.html");
+                startDownloadManager(url, userAgent,
+                           contentDisposition, mimeType,
+                           contentLength);
+
+
+              //  handleExternalStoragePermission();
+//                if (hasExternalStoragePermission()) {
+//                    startDownloadManager(url, userAgent,
+//                            contentDisposition, mimeType,
+//                            contentLength);
+//                } else {
+//                    handleExternalStoragePermission();
+//                }
+            }
+        });
+        browser.loadUrl("https://torrentview15.net");
 
     }
 
+    private void startDownloadManager(String url, String userAgent,
+                                      String contentDisposition, String mimeType,
+                                      long contentLength) {
+        DownloadManager.Request request = new DownloadManager.Request(
+                Uri.parse(url));
+
+
+        request.setMimeType(mimeType);
+
+
+        String cookies = CookieManager.getInstance().getCookie(url);
+
+
+        request.addRequestHeader("cookie", cookies);
+
+
+        request.addRequestHeader("User-Agent", userAgent);
+
+
+        request.setDescription("Downloading file...");
+
+
+        request.setTitle(URLUtil.guessFileName(url, contentDisposition,
+                mimeType));
+
+
+        request.allowScanningByMediaScanner();
+
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(
+                        url, contentDisposition, mimeType));
+        DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        dm.enqueue(request);
+        Toast.makeText(getApplicationContext(), "Downloading File",
+                Toast.LENGTH_LONG).show();
+    }
+
+    private void handleExternalStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )) {
+            showPermissionRationalePopup();
+        } else {
+            requestWriteExternalStoragePermission();
+        }
+
+
+    }
+
+    private boolean hasExternalStoragePermission() {
+        return ActivityCompat.checkSelfPermission(
+                MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+
+            }
+        }
+    }
+
+
+
+    private static final int REQUEST_CODE_REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 4359;
+
+    private void requestWriteExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+                MainActivity.this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                },
+                REQUEST_CODE_REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION
+        );
+    }
+
+    private void showPermissionRationalePopup() {
+        android.app.AlertDialog.Builder alertDialogBuilder = new android.app.AlertDialog.Builder(
+                MainActivity.this);
+        alertDialogBuilder
+                .setMessage("저장용량에 액세스하도록 허용하시겠습니까?")
+                .setCancelable(false)
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                })
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+
+                        requestWriteExternalStoragePermission();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 
 
     /**
